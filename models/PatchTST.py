3adc210cdcb0a9c4fae58248a8143aa04f6c3af6
Xingyu Chen <61250996+xingyu617@users.noreply.github.com>
2023-03-13 16:18:12 +0100

    Add files via upload

--- models/PatchTST.py
+++ models/PatchTST.py
@@ -10,19 +10,40 @@ import numpy as np
 
 from layers.PatchTST_backbone import PatchTST_backbone
 from layers.PatchTST_layers import series_decomp
+from models.patchTST import PatchTSTEncoder,ClassificationHead
+from utils.datautils import *
 
 
+
+class LinearClassificationHead(nn.Module):
+    def __init__(self, n_vars, d_model, n_classes, head_dropout):
+        super().__init__()
+        self.flatten = nn.Flatten(start_dim=1)
+        self.dropout = nn.Dropout(head_dropout)
+        self.linear = nn.Linear(n_vars*d_model, n_classes)
+
+    def forward(self, x):
+        """
+        x: [bs x nvars x d_model x num_patch]
+        output: [bs x n_classes]
+        """
+        #x = x[:,:,:,-1]             # only consider the last item in the sequence, x: bs x nvars x d_model
+        x = self.flatten(x)         # x: bs x nvars * d_model
+        x = self.dropout(x)
+        y = self.linear(x)         # y: bs x n_classes
+        return y
 class Model(nn.Module):
     def __init__(self, configs, max_seq_len:Optional[int]=1024, d_k:Optional[int]=None, d_v:Optional[int]=None, norm:str='BatchNorm', attn_dropout:float=0., 
                  act:str="gelu", key_padding_mask:bool='auto',padding_var:Optional[int]=None, attn_mask:Optional[Tensor]=None, res_attention:bool=True, 
-                 pre_norm:bool=False, store_attn:bool=False, pe:str='zeros', learn_pe:bool=True, pretrain_head:bool=False, head_type = 'flatten', verbose:bool=False, **kwargs):
+                 pre_norm:bool=False, store_attn:bool=False, pe:str='zeros', learn_pe:bool=True, pretrain_head:bool=False, head_type = 'classification', verbose:bool=False, **kwargs):
         
         super().__init__()
         
         # load parameters
         c_in = configs.enc_in
         context_window = configs.seq_len
-        target_window = configs.pred_len
+        #
+        target_window = configs.seq_len
         
         n_layers = configs.e_layers
         n_heads = configs.n_heads
@@ -45,6 +66,9 @@ class Model(nn.Module):
         decomposition = configs.decomposition
         kernel_size = configs.kernel_size
         
+        #patchTST
+        target_dim = configs.class_num
+        shared_embedding = True
         
         # model
         self.decomposition = decomposition
@@ -67,7 +91,7 @@ class Model(nn.Module):
                                   pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
                                   subtract_last=subtract_last, verbose=verbose, **kwargs)
         else:
-            '''
+            
             self.model = PatchTST_backbone(c_in=c_in, context_window = context_window, target_window=target_window, patch_len=patch_len, stride=stride, 
                                   max_seq_len=max_seq_len, n_layers=n_layers, d_model=d_model,
                                   n_heads=n_heads, d_k=d_k, d_v=d_v, d_ff=d_ff, norm=norm, attn_dropout=attn_dropout,
@@ -76,19 +100,16 @@ class Model(nn.Module):
                                   pe=pe, learn_pe=learn_pe, fc_dropout=fc_dropout, head_dropout=head_dropout, padding_patch = padding_patch,
                                   pretrain_head=pretrain_head, head_type=head_type, individual=individual, revin=revin, affine=affine,
                                   subtract_last=subtract_last, verbose=verbose, **kwargs)
-            '''
-            self.backbone = PatchTSTEncoder(c_in, num_patch=num_patch, patch_len=patch_len, 
-                                n_layers=n_layers, d_model=d_model, n_heads=n_heads, 
-                                shared_embedding=shared_embedding, d_ff=d_ff,
-                                attn_dropout=attn_dropout, dropout=dropout, act=act, 
-                                res_attention=res_attention, pre_norm=pre_norm, store_attn=store_attn,
-                                pe=pe, learn_pe=learn_pe, verbose=verbose, **kwargs)
-            # Head
-            self.n_vars = c_in
-            self.head = ClassificationHead(self.n_vars, d_model, target_dim, head_dropout)
+           
+            #self.create_patch = Patch(context_window, patch_len, stride)
+            #self.head = LinearClassificationHead(3,context_window, 2, head_dropout)
 
     
     def forward(self, x):           # x: [Batch, Input length, Channel]
+    
+    
+        #patch time series
+        #x = self.create_patch(x)
         if self.decomposition:
             res_init, trend_init = self.decomp_module(x)
             res_init, trend_init = res_init.permute(0,2,1), trend_init.permute(0,2,1)  # x: [Batch, Channel, Input length]
@@ -99,6 +120,8 @@ class Model(nn.Module):
         else:
             x = x.permute(0,2,1)    # x: [Batch, Channel, Input length]
             x = self.model(x)
-            x = x.permute(0,2,1)    # x: [Batch, Input length, Channel]
-            x = self.head(x)
+            #x = x.permute(0,2,1)    # x: [Batch, Input length, Channel]
+            #print("final output:", x.shape)
+            #x = self.head(x)
+            #print("final class output:", x.shape)
         return x
